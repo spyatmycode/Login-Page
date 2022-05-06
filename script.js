ba1b442f4311c35bf250ef9916c3c28a5751f257
spyatmycode <akejuoluwanifemi11@gmail.com>
2022-05-06 14:14:28 -0700

    updated the password stuff

--- script.js
+++ script.js
@@ -1,7 +1,63 @@
 var loader = document.getElementById('loader');
 
 window.addEventListener(
-    "load", function(){
+    "load", function () {
         document.getElementById('loader').style.display = "none";
     }
 )
+
+
+button = document.getElementById("signin");
+
+// button.addEventListener("click",
+
+//     function(){
+//         if(password == "admin" && username == "admin"){
+//             alert("Login Successful");
+
+//             window.open("http://akejuoluwanifemi-portfolio.netllify.app")
+//         }
+//         else{
+//             alert("Login Failed");
+
+//         }
+
+
+//     }
+
+// )
+
+function verify() {
+
+    var password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+
+    if (password == "admin" && (username == "admin" || username == "akejuoluwanifemi11@gmail.com")) {
+        alert("Login Successful");
+        window.open("http://akejuoluwanifemi-portfolio.netllify.app");
+    }
+    else {
+        alert("Login Failed");
+
+       
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
